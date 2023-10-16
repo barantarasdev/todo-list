f2b80bd727652566ea344a5fb984d934076329ac
Taras Baran <taras.baran@devico.io>
2023-10-16 11:55:08 +0200

    initial-header - Create initial header block, input and button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-const e = '1'
+const header = document.querySelector('.header')
+const form = header.querySelector('.header__form')
+const input = header.querySelector('.header__input')
+
+const todos = document.querySelector('.todos')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const value = input.value
+
+  if (value.length <= 0) {
+    input.classList.add('header__input--red')
+
+    return
+  }
+
+  input.classList.remove('header__input--red')
+
+  todos.insertAdjacentHTML(
+    'beforeend',
+    `
+      <li>${value}</li>
+    `,
+  )
+
+  input.value = ''
+})
